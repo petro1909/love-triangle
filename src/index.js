d07b074e0058348cbde0523b073bee3eb571d0fa
petro1909 <petya-97_97@mail.ru>
2019-03-20 21:01:24 +0300

    feat: solve

--- src/index.js
+++ src/index.js
@@ -4,4 +4,17 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var count = 0;  
+  for (var i = 0; i<preferences.length; i++) { 
+      var n = preferences[i] - 1;
+      if (n == i) continue;
+      var k = preferences[n] - 1;
+      if (n == k) continue;
+      var m = preferences[k] - 1;
+      if (m == k) continue;
+      if (m == i) {
+          count++;
+      }
+   }
+   return count / 3;
 };
